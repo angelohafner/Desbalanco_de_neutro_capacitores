3c7274291d2449d58c4b0ef03ef0a7f7cd9b05b1
angel <angelo.hafner@gmail.com>
2023-06-25 15:53:41 -0300

    quando pensei no domingo, se der até aqui sem erro paro... e assim fiz.

--- funcoes_desbalanco_neutro.py
+++ funcoes_desbalanco_neutro.py
@@ -11,7 +11,7 @@ from openpyxl.formatting.rule import Rule, ColorScaleRule
 
 
 # %%
-def destaca_maiores_que_nominal(planilha='Ia1', aquivo='correntes_nas_latas.xlsx', valor_nominal=10.1):
+def destaca_maiores_que_nominal(planilha='Ia1', aquivo='correntes.xlsx', valor_nominal=10.1):
     wb = load_workbook(aquivo)
     ws = wb[planilha]
     red_fill = PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
@@ -89,47 +89,80 @@ def matriz_fases_ramos(nr_lin_ext, nr_col_ext, nr_lin_int, nr_col_int, cap_inter
     return wb, matriz
 
 # %%
-def matrizes_internas(matriz_FR, nr_lin_ext, nr_col_ext, nr_lin_int,  nr_col_int, ramo=99):
+def matrizes_internas(matriz_FR, nr_lin_ext, nr_col_ext, nr_lin_int,  nr_col_int):
     super_matriz_FR = np.ones((nr_lin_ext, nr_col_ext, nr_lin_int, nr_col_int))
-    eq_paral_internos = np.ones((nr_lin_ext * nr_lin_int, nr_col_ext))
+    eq_paral_internos = np.ones((nr_lin_ext, nr_col_ext, nr_lin_int))
     eq_serie_internos = np.ones((nr_lin_ext, nr_col_ext))
     for i_ext in range(nr_lin_ext):
         for j_ext in range(nr_col_ext):
-            row_sart = i_ext * nr_lin_int
-            row_end = row_sart + nr_lin_int
-            col_sart = j_ext * nr_col_int
-            col_end = col_sart + nr_col_int
-            # matriz interna temporária
-            mat_temp = np.array(matriz_FR[row_sart:row_end, col_sart:col_end])
-            super_matriz_FR[i_ext, j_ext] = mat_temp
-            eq_paral_internos[i_ext * nr_lin_int:i_ext * nr_lin_int + nr_lin_int, j_ext] = np.sum(mat_temp, axis=1)
-            eq_serie_internos[i_ext, j_ext] = 1 / np.sum(1 / eq_paral_internos[i_ext * nr_lin_int:i_ext * nr_lin_int + nr_lin_int, j_ext])
+            rs = i_ext * nr_lin_int
+            re = rs + nr_lin_int
+            cs = j_ext * nr_col_int
+            ce = cs + nr_col_int
+            super_matriz_FR[i_ext, j_ext, :, :] = np.array(matriz_FR[rs:re, cs:ce])
+            sbmatriz = super_matriz_FR[i_ext, j_ext, :, :]
+            eq_paral_internos[i_ext, j_ext, :] = np.sum(sbmatriz, axis=1)
+            eq_serie_internos[i_ext, j_ext] = 1 / np.sum(1 / eq_paral_internos[i_ext, j_ext, :])
 
     return super_matriz_FR, eq_paral_internos, eq_serie_internos
 
 # %%
-def calcular_corrente_tensao(V_ao, omega, eq_serie_externos_A1, eq_paral_externos_A1, eq_serie_internos_A1):
+def calcular_corrente_tensao(V_ao, omega, eq_serie_externos_A1, eq_paral_externos_A1, eq_serie_internos_A1, eq_paral_internos_A1, matriz_A1):
     I_a1 = V_ao * (1j*omega*eq_serie_externos_A1)
     V_a1_ser = I_a1 * 1 / (1j*omega*eq_paral_externos_A1)
-    V_a1_ser = V_a1_ser.reshape(-1, 1) # necessário para transformar em matriz com uma coluna
+    # V_a1_ser = V_a1_ser.reshape(-1, 1) # necessário para transformar em matriz com uma coluna
     I_a1_par = V_a1_ser * (1j*omega*eq_serie_internos_A1)
-    return I_a1, V_a1_ser, I_a1_par
+    V_a1_ser_int = I_a1_par * 1 / (1j*omega*eq_paral_internos_A1)
+    I_a1_par_int = V_a1_ser_int * (1j*omega*matriz_A1)
+    return I_a1, V_a1_ser, I_a1_par, V_a1_ser_int, I_a1_par_int
 
 # %%
-def calcular_e_concatenar(V_ao, omega, eq_serie_externos_A1, eq_paral_externos_A1, eq_serie_internos_A1, eq_serie_externos_A2, eq_paral_externos_A2, eq_serie_internos_A2, eq_serie_externos_B1, eq_paral_externos_B1, eq_serie_internos_B1, eq_serie_externos_B2, eq_paral_externos_B2, eq_serie_internos_B2, eq_serie_externos_C1, eq_paral_externos_C1, eq_serie_internos_C1, eq_serie_externos_C2, eq_paral_externos_C2, eq_serie_internos_C2):
-    I_a1, V_a1_ser, I_a1_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_A1, eq_paral_externos_A1, eq_serie_internos_A1)
-    I_a2, V_a2_ser, I_a2_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_A2, eq_paral_externos_A2, eq_serie_internos_A2)
-    I_a_par = np.concatenate((I_a1, I_a2), axis=1)
-    df_I_a_par = pd.DataFrame(I_a_par)
+def colorir_planilha_especifica(wb, planilha, nr_lin_int, nr_col_int, nr_lin_ext, nr_col_ext):
 
-    I_b1, V_b1_ser, I_b1_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_B1, eq_paral_externos_B1, eq_serie_internos_B1)
-    I_b2, V_b2_ser, I_b2_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_B2, eq_paral_externos_B2, eq_serie_internos_B2)
-    I_b_par = np.concatenate((I_b1, I_b2), axis=1)
-    df_I_b_par = pd.DataFrame(I_b_par)
+    cores = ['FFFFCC', 'CCFFCC', 'CCCCFF', 'FFCCFF']
+    ws = wb[planilha]
+    for i_ext in range(nr_lin_ext):
+        for j_ext in range(2 * nr_col_ext):
+            cor = cores[(i_ext + j_ext) % len(cores)]
+            fill = PatternFill(start_color=cor, end_color=cor, fill_type='solid')
+            for i_int in range(nr_lin_int):
+                for j_int in range(nr_col_int):
+                    ii = i_ext * nr_lin_int + i_int
+                    jj = j_ext * nr_col_int + j_int
+                    ws.row_dimensions[ii + 1].height = 30
+                    ws.cell(row=ii + 1, column=jj + 1).alignment = Alignment(horizontal='center', vertical='center')
+                    if j_ext >= nr_col_ext:
+                        ws.cell(row=ii + 1, column=jj + 1).font = Font(name='Bahnschrift SemiBold Condensed', color="FF0000", underline="single")
+                    else:
+                        ws.cell(row=ii + 1, column=jj + 1).font = Font(name='Bahnschrift SemiBold Condensed', color="0000FF")
 
-    I_c1, V_c1_ser, I_c1_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_C1, eq_paral_externos_C1, eq_serie_internos_C1)
-    I_c2, V_c2_ser, I_c2_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_C2, eq_paral_externos_C2, eq_serie_internos_C2)
-    I_c_par = np.concatenate((I_c1, I_c2), axis=1)
-    df_I_c_par = pd.DataFrame(I_c_par)
+    wb.save('correntes.xlsx')
 
-    return [df_I_a_par, df_I_b_par, df_I_c_par]
+# %%
+def fase(matriz, nr_col_ext, nr_col_int, nr_lin_ext, nr_lin_int, ramo1, ramo2):
+    matriz1 = matriz[:, :nr_col_ext * nr_col_int]
+    matriz2 = matriz[:, nr_col_ext * nr_col_int:]
+
+    super_matriz1, eq_paral_internos1, eq_serie_internos1 = matrizes_internas(matriz_FR=matriz1, nr_lin_ext=nr_lin_ext, nr_col_ext=nr_col_ext, nr_lin_int=nr_lin_int, nr_col_int=nr_col_int)
+    super_matriz2, eq_paral_internos2, eq_serie_internos2 = matrizes_internas(matriz_FR=matriz2, nr_lin_ext=nr_lin_ext, nr_col_ext=nr_col_ext, nr_lin_int=nr_lin_int, nr_col_int=nr_col_int)
+
+    eq_paral_externos1 = (np.sum(eq_serie_internos1, axis=1)).reshape(-1, 1)
+    eq_serie_externos1 = 1 / np.sum(1 / eq_paral_externos1)
+
+    eq_paral_externos2 = (np.sum(eq_serie_internos2, axis=1)).reshape(-1, 1)
+    eq_serie_externos2 = 1 / np.sum(1 / eq_paral_externos2)
+
+    eq_paral_ramos = eq_serie_externos1 + eq_serie_externos2
+
+    return [eq_paral_ramos, eq_serie_externos1, eq_paral_externos1, eq_serie_internos1, eq_paral_internos1, eq_serie_externos2, eq_paral_externos2, eq_serie_internos2, eq_paral_internos2, matriz1, matriz2, super_matriz1, super_matriz2]
+# %%
+def calcular_correntes_tensoes(Za, Zb, Zc, Vff, a, matriz_impedancia_sistema):
+    matriz_impedancia_malha = np.array([[Za+Zb, -Zb], [-Zb, Zb+Zc]])
+    matriz_fontes_malha = np.array([[Vff], [Vff*(a**2)]])
+    matriz_correntes_alphabeta = np.linalg.inv(matriz_impedancia_malha) @ matriz_fontes_malha
+    I_alpha = matriz_correntes_alphabeta[0, 0]
+    I_beta = matriz_correntes_alphabeta[1, 0]
+    matriz_correntes_fase = np.array([[I_alpha], [I_beta-I_alpha], [-I_beta]])
+    matriz_tensoes_Vabco = matriz_impedancia_sistema @ matriz_correntes_fase
+    tensao_deslocamento_netro = np.sum(matriz_tensoes_Vabco) / 3
+    return matriz_correntes_fase, matriz_tensoes_Vabco, tensao_deslocamento_netro
--- temp.py
+++ temp.py
@@ -1,40 +1,38 @@
-# import os
-# import numpy as np
-# import pandas as pd
-# from openpyxl import load_workbook, Workbook
-# from openpyxl.utils import get_column_letter
-# from openpyxl.utils import get_column_letter
-# from openpyxl.styles import PatternFill, Font, Color, Alignment
-# from openpyxl.styles.differential import DifferentialStyle
-# from openpyxl.formatting.rule import Rule, ColorScaleRule
+import os
+import numpy as np
+import pandas as pd
+from openpyxl import load_workbook, Workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import PatternFill, Font, Color, Alignment
+from openpyxl.styles.differential import DifferentialStyle
+from openpyxl.formatting.rule import Rule, ColorScaleRule
 from engineering_notation import EngNumber
 from funcoes_desbalanco_neutro import *
 
 
 # %%
-
-a = 1*np.exp(-1j*2*np.pi/3)
-
-nr_lin_int = 1
-nr_col_int = 1
-
-nr_lin_ext = 1
-nr_col_ext = 1
+a = 1*np.exp(1j*2*np.pi/3)
+nr_lin_int = 5
+nr_col_int = 4
+nr_lin_ext = 3
+nr_col_ext = 2
 
 potencia_nominal_trifásica = 10e6
 omega = 2*np.pi*60
 tensao_nominal_fase_fase = 69e3
-Vff = tensao_nominal_fase_fase*np.sqrt(3)*np.exp(+1j*np.pi/6)
-tensao_fase_neutro = tensao_nominal_fase_fase/np.sqrt(3)
-corrente_fase_neutro = ( potencia_nominal_trifásica/3 ) / tensao_fase_neutro
+Vff = tensao_nominal_fase_fase * np.exp(1j*np.pi/6)
+tensao_fase_neutro = tensao_nominal_fase_fase / np.sqrt(3)
+corrente_fase_neutro = (potencia_nominal_trifásica/3) / tensao_fase_neutro
 reatancia = tensao_fase_neutro / corrente_fase_neutro
-corrente_nominal_lata = corrente_fase_neutro / nr_col_ext
+corrente_nominal_lata = corrente_fase_neutro
 
-cap_interna = 1 / (omega*reatancia)
+cap_total = 1 / (omega*reatancia)
+cap_interna = cap_total * (nr_lin_int + nr_lin_ext) / (nr_col_int + nr_col_ext)
 des_int = 0.0
 
-print("corrente_nominal_lata = corrente_fase_neutro / nr_col_ext = ", corrente_nominal_lata)
-print("cap_interna = 1 / (omega*reatancia) = ", EngNumber(cap_interna))
+print("cap_total = ", cap_total)
+print("cap_interna*2 = ", EngNumber(2*cap_interna))
 
 # %% gerar matriz no excel
 wb, matriz = matriz_fases_ramos(nr_lin_ext=nr_lin_ext, nr_col_ext=nr_col_ext, nr_lin_int=nr_lin_int, nr_col_int=nr_col_int, cap_interna=cap_interna, des_int=des_int)
@@ -46,51 +44,33 @@ matriz_A = data['A']
 matriz_B = data['B']
 matriz_C = data['C']
 
-matriz_A1 = matriz_A[:, :nr_col_ext*nr_col_int]
-matriz_A2 = matriz_A[:, nr_col_ext*nr_col_int:]
-super_matriz_A1, eq_paral_internos_A1, eq_serie_internos_A1 = matrizes_internas(matriz_FR=matriz_A1, nr_lin_ext=nr_lin_ext, nr_col_ext=nr_col_ext, nr_lin_int=nr_lin_int,  nr_col_int=nr_lin_int, ramo=1)
-super_matriz_A2, eq_paral_internos_A2, eq_serie_internos_A2 = matrizes_internas(matriz_FR=matriz_A2, nr_lin_ext=nr_lin_ext, nr_col_ext=nr_col_ext, nr_lin_int=nr_lin_int,  nr_col_int=nr_lin_int, ramo=2)
-eq_paral_externos_A1 = np.sum(eq_serie_internos_A1, axis=1)
-eq_serie_externos_A1 = 1 / np.sum(1 / eq_paral_externos_A1)
-eq_paral_externos_A2 = np.sum(eq_serie_internos_A2, axis=1)
-eq_serie_externos_A2 = 1 / np.sum(1 / eq_paral_externos_A2)
-eq_paral_ramos_A = eq_serie_externos_A1 + eq_serie_externos_A2
 
+# %%
+eq_paral_ramos_A, \
+    eq_serie_externos_A1, eq_paral_externos_A1, eq_serie_internos_A1, eq_paral_internos_A1, \
+    eq_serie_externos_A2, eq_paral_externos_A2, eq_serie_internos_A2, eq_paral_internos_A2, \
+    matriz_A1, matriz_A2, super_matriz_A1, super_matriz_A2 = \
+    fase(matriz_A, nr_col_ext, nr_col_int, nr_lin_ext, nr_lin_int, 1, 2)
 
-matriz_B1 = matriz_B[:, :nr_col_ext*nr_col_int]
-matriz_B2 = matriz_B[:, nr_col_ext*nr_col_int:]
-super_matriz_B1, eq_paral_internos_B1, eq_serie_internos_B1 = matrizes_internas(matriz_FR=matriz_B1, nr_lin_ext=nr_lin_ext, nr_col_ext=nr_col_ext, nr_lin_int=nr_lin_int,  nr_col_int=nr_lin_int, ramo=1)
-super_matriz_B2, eq_paral_internos_B2, eq_serie_internos_B2 = matrizes_internas(matriz_FR=matriz_B2, nr_lin_ext=nr_lin_ext, nr_col_ext=nr_col_ext, nr_lin_int=nr_lin_int,  nr_col_int=nr_lin_int, ramo=2)
-eq_paral_externos_B1 = np.sum(eq_serie_internos_B1, axis=1)
-eq_serie_externos_B1 = 1 / np.sum(1 / eq_paral_externos_B1)
-eq_paral_externos_B2 = np.sum(eq_serie_internos_B2, axis=1)
-eq_serie_externos_B2 = 1 / np.sum(1 / eq_paral_externos_B2)
-eq_paral_ramos_B = eq_serie_externos_B1 + eq_serie_externos_B2
+eq_paral_ramos_B, \
+    eq_serie_externos_B1, eq_paral_externos_B1, eq_serie_internos_B1, eq_paral_internos_B1, \
+    eq_serie_externos_B2, eq_paral_externos_B2, eq_serie_internos_B2, eq_paral_internos_B2, \
+    matriz_B1, matriz_B2, super_matriz_B1, super_matriz_B2 = \
+    fase(matriz_B, nr_col_ext, nr_col_int, nr_lin_ext, nr_lin_int, 1, 2)
 
-
-matriz_C1 = matriz_C[:, :nr_col_ext*nr_col_int]
-matriz_C2 = matriz_C[:, nr_col_ext*nr_col_int:]
-super_matriz_C1, eq_paral_internos_C1, eq_serie_internos_C1 = matrizes_internas(matriz_FR=matriz_C1, nr_lin_ext=nr_lin_ext, nr_col_ext=nr_col_ext, nr_lin_int=nr_lin_int,  nr_col_int=nr_lin_int, ramo=1)
-super_matriz_C2, eq_paral_internos_C2, eq_serie_internos_C2 = matrizes_internas(matriz_FR=matriz_C2, nr_lin_ext=nr_lin_ext, nr_col_ext=nr_col_ext, nr_lin_int=nr_lin_int,  nr_col_int=nr_lin_int, ramo=2)
-eq_paral_externos_C1 = np.sum(eq_serie_internos_C1, axis=1)
-eq_serie_externos_C1 = 1 / np.sum(1 / eq_paral_externos_C1)
-eq_paral_externos_C2 = np.sum(eq_serie_internos_C2, axis=1)
-eq_serie_externos_C2 = 1 / np.sum(1 / eq_paral_externos_C2)
-eq_paral_ramos_C = eq_serie_externos_C1 + eq_serie_externos_C2
-
+eq_paral_ramos_C, \
+    eq_serie_externos_C1, eq_paral_externos_C1, eq_serie_internos_C1, eq_paral_internos_C1, \
+    eq_serie_externos_C2, eq_paral_externos_C2, eq_serie_internos_C2, eq_paral_internos_C2, \
+    matriz_C1, matriz_C2, super_matriz_C1, super_matriz_C2 = \
+    fase(matriz_C, nr_col_ext, nr_col_int, nr_lin_ext, nr_lin_int, 1, 2)
 
 Za = 1 / (1j*omega*eq_paral_ramos_A)
 Zb = 1 / (1j*omega*eq_paral_ramos_B)
 Zc = 1 / (1j*omega*eq_paral_ramos_C)
 
+# %%
 matriz_impedancia_sistema = np.array([[Za, 0, 0], [0, Zb, 0], [0, 0, Zc]])
-
-matriz_impedancia_malha = np.array([[Za, -Zb, 0], [0, Zb, -Zc], [1, 1, 1]])
-matriz_fontes_malha = np.array([[Vff], [Vff*a], [0]])
-matriz_correntes_fase = np.linalg.inv(matriz_impedancia_malha) @ matriz_fontes_malha
-
-matriz_tensoes_Vabco = matriz_impedancia_sistema @ matriz_correntes_fase
-tensao_deslocamento_netro = np.sum(matriz_tensoes_Vabco) / 3
+matriz_correntes_fase, matriz_tensoes_Vabco, tensao_deslocamento_netro = calcular_correntes_tensoes(Za, Zb, Zc, Vff, a, matriz_impedancia_sistema)
 
 V_ao = matriz_tensoes_Vabco[0]
 V_bo = matriz_tensoes_Vabco[1]
@@ -99,44 +79,117 @@ V_co = matriz_tensoes_Vabco[2]
 I_ao = matriz_correntes_fase[0]
 I_bo = matriz_correntes_fase[1]
 I_co = matriz_correntes_fase[2]
+#
+# # %% Começa o caminho inverso
+#
+# #%% Fase A
+# df_Ca_eq_paral_ramos_A = pd.DataFrame([eq_paral_ramos_A])
+# df_Ca_eq_ext = pd.DataFrame(np.append(eq_serie_externos_A1, eq_serie_externos_A2))
+# df_Ca_pa_ext = pd.DataFrame(np.append(eq_paral_externos_A1, eq_paral_externos_A2, axis=1))
+# df_Ca_eq_int = pd.DataFrame(np.append(eq_serie_internos_A1, eq_serie_internos_A2, axis=1))
+# df_Ca_pa_int = pd.DataFrame(np.append(eq_paral_internos_A1, eq_paral_internos_A2, axis=1))
+# df_Ca__todos = pd.DataFrame(matriz_A)
+#
+#
+# with pd.ExcelWriter('capacitancias_nas_latas.xlsx', engine='openpyxl') as writer:
+#     df_Ca_eq_ext.T.to_excel(writer, sheet_name='df_Ca_eq_ext', index=False, header=False)
+#     df_Ca_pa_ext.to_excel(writer, sheet_name='df_Ca_pa_ext', index=False, header=False)
+#     df_Ca_eq_int.to_excel(writer, sheet_name='df_Ca_eq_int', index=False, header=False)
+#     df_Ca_pa_int.to_excel(writer, sheet_name='df_Ca_pa_int', index=False, header=False)
+#     df_Ca__todos.to_excel(writer, sheet_name='df_Ca__todos', index=False, header=False)
+#
+
+# corrente do capacitor equivalente
+I_a1_ext = V_ao * (1j*omega*eq_serie_externos_A1)
+# tensoes dos capacitores que estao em serie
+V_a1_ser_ext = I_a1_ext / (1j*omega*eq_paral_externos_A1)
+# corrente dos capacitores que estao em paralelo
+I_a1_par_ext = V_a1_ser_ext * (1j*omega*eq_serie_internos_A1)
+
+# AGORA VAMOS PARA OS INTERNOS
+I_a1_ser_int = np.ones((nr_lin_ext, nr_col_ext, nr_lin_int, nr_col_int), dtype='complex')
+V_a1_ser_int = np.ones((nr_lin_ext, nr_col_ext, nr_lin_int), dtype='complex')
+for i_ext in range(nr_lin_ext):
+    for j_ext in range(nr_col_ext):
+        # capacitores internos em série
+        V_a1_ser_int[i_ext, j_ext, :] = I_a1_par_ext[i_ext, j_ext] / (1j * omega * eq_paral_internos_A1[i_ext, j_ext, :])
 
 
-# %% Começa o caminho inverso
-# %% Fase A
-
-df_Ca_eq_ext = pd.DataFrame([eq_serie_externos_A1, eq_serie_externos_A2])
-df_Ca_pa_ext = pd.DataFrame([eq_paral_externos_A1, eq_paral_externos_A2])
-df_Ca_eq_int = pd.DataFrame(np.append(eq_serie_internos_A1, eq_serie_internos_A2, axis=1))
 
 
-with pd.ExcelWriter('capacitancias_nas_latas.xlsx', engine='openpyxl') as writer:
-    df_Ca_eq_ext.T.to_excel(writer, sheet_name='df_Ca_eq_ext', index=False, header=False)
-    df_Ca_pa_ext.T.to_excel(writer, sheet_name='df_Ca_pa_ext', index=False, header=False)
-    df_Ca_eq_int.to_excel(writer, sheet_name='df_Ca_eq_int', index=False, header=False)
 
-# %%
-I_a1, V_a1_ser, I_a1_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_A1, eq_paral_externos_A1, eq_serie_internos_A1)
-I_a2, V_a2_ser, I_a2_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_A2, eq_paral_externos_A2, eq_serie_internos_A2)
-I_a_par = np.hstack((I_a1_par, I_a2_par))
-df_I_a_par = pd.DataFrame(np.abs(I_a_par))
 
-I_b1, V_b1_ser, I_b1_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_B1, eq_paral_externos_B1, eq_serie_internos_B1)
-I_b2, V_b2_ser, I_b2_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_B2, eq_paral_externos_B2, eq_serie_internos_B2)
-I_b_par = np.hstack((I_b1_par, I_b2_par))
-df_I_b_par = pd.DataFrame(np.abs(I_b_par))
 
-I_c1, V_c1_ser, I_c1_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_C1, eq_paral_externos_C1, eq_serie_internos_C1)
-I_c2, V_c2_ser, I_c2_par = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_C2, eq_paral_externos_C2, eq_serie_internos_C2)
-I_c_par = np.hstack((I_c1_par, I_c2_par))
-df_I_c_par = pd.DataFrame(np.abs(I_c_par))
 
-with pd.ExcelWriter('correntes_nas_latas.xlsx', engine='openpyxl') as writer:
-    df_I_a_par.to_excel(writer, sheet_name='df_I_a_par', index=False, header=False)
-    df_I_b_par.to_excel(writer, sheet_name='df_I_b_par', index=False, header=False)
-    df_I_c_par.to_excel(writer, sheet_name='df_I_c_par', index=False, header=False)
 
-destaca_maiores_que_nominal(planilha='df_I_a_par', aquivo='correntes_nas_latas.xlsx', valor_nominal=corrente_nominal_lata)
-destaca_maiores_que_nominal(planilha='df_I_b_par', aquivo='correntes_nas_latas.xlsx', valor_nominal=corrente_nominal_lata)
-destaca_maiores_que_nominal(planilha='df_I_c_par', aquivo='correntes_nas_latas.xlsx', valor_nominal=corrente_nominal_lata)
-# %%
 
+
+
+
+
+
+
+# V_a1_ser_int = np.ones((nr_lin_ext, nr_col_ext, nr_lin_int, nr_col_ext), dtype='complex')
+# I_a1_par_int = np.ones((nr_lin_ext, nr_col_ext, nr_lin_int, nr_col_int), dtype='complex')
+# V_a1_ser_int_para_excel = np.ones((nr_lin_ext*nr_lin_int, nr_col_ext), dtype='complex')
+# I_a1_par_int_para_excel = np.ones((nr_lin_ext*nr_lin_int, nr_col_int), dtype='complex')
+
+# para cada conjunto interno equipavlente paralelo (que está em série)
+# for i_ext in range(nr_lin_ext):
+#     rs = i_ext * nr_lin_int
+#     re = rs + nr_lin_int
+#     for j_ext in range(nr_col_ext):
+#         V_a1_ser_int[i_ext, j_ext, :, :] = I_a1_par[i_ext, j_ext] * 1 / (1j*omega*eq_paral_internos_A1[rs:re, 0:nr_col_ext])
+#         V_a1_ser_int_para_excel[rs:re, 0:nr_col_ext] = I_a1_par[i_ext, j_ext] * 1 / (1j*omega*eq_paral_internos_A1[rs:re, 0:nr_col_ext])
+#
+# for i_ext in range(nr_lin_ext):
+#     rs = i_ext * nr_lin_int
+#     re = rs + nr_lin_int
+#     I_a1_par_int[i_ext, 0:nr_col_int, :, :] = V_a1_ser_int[i_ext, 0:nr_col_int, :, :] * (1j*omega*super_matriz_A1[rs, re, :, :])
+#     # I_a1_par_int_para_excel[rs:re, cs:ce] = V_a1_ser_int[i_ext, j_ext, :, :] * (1j*omega*matriz_A1[rs:re, cs:ce])
+
+
+
+# I_a1, V_a1_ser, I_a1_par, V_a1_ser_int, I_a1_par_int = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_A1, eq_paral_externos_A1, eq_serie_internos_A1, eq_paral_internos_A1, matriz_A1)
+# I_a2, V_a2_ser, I_a2_par, V_a2_ser_int, I_a2_par_int = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_A2, eq_paral_externos_A2, eq_serie_internos_A2, eq_paral_internos_A2, matriz_A2)
+# I_a_par = np.hstack((I_a1_par, I_a2_par))
+# df_I_a_par = pd.DataFrame(np.abs(I_a_par))
+# I_a_par_int = np.hstack((I_a1_par_int, I_a2_par_int))
+# df_I_a_par_int = pd.DataFrame(np.abs(I_a_par_int))
+#
+# I_b1, V_b1_ser, I_b1_par, V_b1_ser_int, I_b1_par_int = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_B1, eq_paral_externos_B1, eq_serie_internos_B1, eq_paral_internos_B1, matriz_B1)
+# I_b2, V_b2_ser, I_b2_par, V_b2_ser_int, I_b2_par_int = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_B2, eq_paral_externos_B2, eq_serie_internos_B2, eq_paral_internos_B2, matriz_B2)
+# I_b_par = np.hstack((I_b1_par, I_b2_par))
+# df_I_b_par = pd.DataFrame(np.abs(I_b_par))
+# I_b_par_int = np.hstack((I_b1_par_int, I_b2_par_int))
+# df_I_b_par_int = pd.DataFrame(np.abs(I_b_par_int))
+#
+# I_c1, V_c1_ser, I_c1_par, V_c1_ser_int, I_c1_par_int = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_C1, eq_paral_externos_C1, eq_serie_internos_C1, eq_paral_internos_C1, matriz_C1)
+# I_c2, V_c2_ser, I_c2_par, V_c2_ser_int, I_c2_par_int = calcular_corrente_tensao(V_ao, omega, eq_serie_externos_C2, eq_paral_externos_C2, eq_serie_internos_C2, eq_paral_internos_C2, matriz_C2)
+# I_c_par = np.hstack((I_c1_par, I_c2_par))
+# df_I_c_par = pd.DataFrame(np.abs(I_c_par))
+# I_c_par_int = np.hstack((I_c1_par_int, I_c2_par_int))
+# df_I_c_par_int = pd.DataFrame(np.abs(I_c_par_int))
+#
+# with pd.ExcelWriter('correntes.xlsx', engine='openpyxl') as writer:
+#     df_I_a_par.to_excel(writer, sheet_name='df_I_a_par', index=False, header=False)
+#     df_I_b_par.to_excel(writer, sheet_name='df_I_b_par', index=False, header=False)
+#     df_I_c_par.to_excel(writer, sheet_name='df_I_c_par', index=False, header=False)
+#     df_I_a_par_int.to_excel(writer, sheet_name='df_I_a_par_int', index=False, header=False)
+#     df_I_b_par_int.to_excel(writer, sheet_name='df_I_b_par_int', index=False, header=False)
+#     df_I_c_par_int.to_excel(writer, sheet_name='df_I_c_par_int', index=False, header=False)
+#
+# destaca_maiores_que_nominal(planilha='df_I_a_par', aquivo='correntes.xlsx', valor_nominal=corrente_nominal_lata)
+# destaca_maiores_que_nominal(planilha='df_I_b_par', aquivo='correntes.xlsx', valor_nominal=corrente_nominal_lata)
+# destaca_maiores_que_nominal(planilha='df_I_c_par', aquivo='correntes.xlsx', valor_nominal=corrente_nominal_lata)
+#
+#
+# wb = load_workbook('correntes.xlsx')
+# colorir_planilha_especifica(wb, 'df_I_a_par_int', nr_lin_int, nr_col_int, nr_lin_ext, nr_col_ext)
+# colorir_planilha_especifica(wb, 'df_I_b_par_int', nr_lin_int, nr_col_int, nr_lin_ext, nr_col_ext)
+# colorir_planilha_especifica(wb, 'df_I_c_par_int', nr_lin_int, nr_col_int, nr_lin_ext, nr_col_ext)
+#
+#
+#
+# # %%
+#
